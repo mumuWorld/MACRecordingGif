eb8119fe8385ec28fb9ecf5ceb4d0543392a4af6
木木 <617958070@qq.com>
2020-05-14 18:48:07 +0800

    -功能[ first ] -备注[ ]

---
+++ RecordingGif/RecordingGif/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RecordingGif
+//
+//  Created by mumu on 2020/5/14.
+//  Copyright © 2020 Mumu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
